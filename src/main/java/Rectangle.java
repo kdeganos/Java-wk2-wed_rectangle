9e4caf28a4497c2be1727139fd4069b550ff73e6
Daniel Wulff and Kevin Deganos <kevin@>
2016-04-27 09:08:22 -0700

    started with mondays rectangle lesson files

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,22 @@
+public class Rectangle {
+  private int mlength;
+  private int mwidth;
+
+  public Rectangle(int length, int width) {
+    mlength = length;
+    mwidth = width;
+  }
+
+  public int getLength() {
+    return mlength;
+  }
+
+  public int getWidth() {
+    return mwidth;
+  }
+
+  public boolean isSquare() {
+    return (mwidth == mlength);
+  }
+
+}
